fd3c39b6bd51166e19f353fdfb6a257e4cf93db4
TanimizuShotaro <e8908@maizuru.kosen-ac.jp>
2022-07-11 19:30:36 +0900

    フォームにコメント付けたりしたよ

--- resources/views/ANNDB.blade.php
+++ resources/views/ANNDB.blade.php
@@ -1,8 +1,8 @@
-<form action="">
+<form action="/DBtest" method="POST"> <!--actionがURL指定的なの、method="POST"は何かデータを送るためのページですよと指定する奴-->
     @csrf <!--これ書いといかんとバグ？るかもって。-->
     <input type="text" name="name" placeholder="What's your name?" required>
     <input type="text" name="age" placeholder="How old are you?" required>
-    <input type="text" name="any" placeholder="コメントしてね" required>
+    <input type="text" name="any" placeholder="コメントしてね" required><!-- typeは何を入力するか？nameはテーブル上の何というカラムにデータを送るか、placeholderは何も入力されてないときに表示する奴、requiredは未入力じゃ受け付けませんって奴-->
 
     <input type="submit">
 </form>
