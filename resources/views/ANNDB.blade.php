8baf55ad91859efaec41f351219aaddc27fafea1
TanimizuShotaro <e8908@maizuru.kosen-ac.jp>
2022-07-11 19:24:23 +0900

    フォーム作ったよ

--- resources/views/ANNDB.blade.php
+++ resources/views/ANNDB.blade.php
@@ -1,1 +1,8 @@
-<form action=""></form>
+<form action="">
+    @csrf <!--これ書いといかんとバグ？るかもって。-->
+    <input type="text" name="name" placeholder="What's your name?" required>
+    <input type="text" name="age" placeholder="How old are you?" required>
+    <input type="text" name="any" placeholder="コメントしてね" required>
+
+    <input type="submit">
+</form>
