c947d0611088ea7d15cf0cc2d286c88daf82713a
TanimizuShotaro <e8908@maizuru.kosen-ac.jp>
2022-07-11 19:58:48 +0900

    コントローラ試作したお

--- app/Http/Controllers/Test1Controller.php
+++ app/Http/Controllers/Test1Controller.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-
+use App\Models\Test1;
 
 class Test1Controller extends Controller
 {
-    //
+    public function yamato(Request $request)
+    {
+        $test1 = new Test1();
+        $test1->name = $request->name;
+        $test1->age = $request->age;
+        $test1->any = $request->any;
+        $test1->save();
+        return redirect('/');
+    }
 }
--- app/Models/Test1.php
+++ app/Models/Test1.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Test1 extends Model
 {
     use HasFactory;
+    protected $guarded = ['create_at', 'update_at'];
 }
