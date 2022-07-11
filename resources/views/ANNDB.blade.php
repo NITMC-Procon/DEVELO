2c0e69a98f020e38b46c11ebe97208c57e52295f
TanimizuShotaro <e8908@maizuru.kosen-ac.jp>
2022-07-11 20:18:15 +0900

    フォームとデータベースの連携完成！！！！

--- resources/views/ANNDB.blade.php
+++ resources/views/ANNDB.blade.php
@@ -1,4 +1,4 @@
-<form action="/DBtest" method="POST"> <!--actionがURL指定的なの、method="POST"は何かデータを送るためのページですよと指定する奴-->
+<form action="/DBtest1" method="POST"> <!--actionがURL指定的なの、method="POST"は何かデータを送るためのページですよと指定する奴-->
     @csrf <!--これ書いといかんとバグ？るかもって。-->
     <input type="text" name="name" placeholder="What's your name?" required>
     <input type="text" name="age" placeholder="How old are you?" required>
